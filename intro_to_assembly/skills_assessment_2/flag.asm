3762c3055c2bbb763f41afa2b32e57285dd60d5e
H@ck3rDan <bill@fastmolasses.us>
2026-01-16 17:11:50 -0500

    Skills assesment 2 done

---
+++ intro_to_assembly/skills_assessment_2/flag.asm
@@ -0,0 +1,39 @@
+global _start
+
+section .text
+_start:
+    ; push './flg.txt\x00'
+    ; this will cause a NULL byte
+   push 0              ; push NULL string terminator
+    
+    
+    mov rdi, '/flg.txt' ; rest of file name
+    push rdi            ; push to stack 
+    
+    ; open('rsp', 'O_RDONLY')
+    mov rax, 2          ; open syscall number
+    mov rdi, rsp        ; move pointer to filename
+    xor rsi, rsi 
+    ;mov rsi, 0          ; set O_RDONLY flag
+    syscall
+
+    ; read file
+    lea rsi, [rdi]      ; pointer to opened file
+    mov rdi, rax        ; set fd to rax from open syscall
+    xor rax, rax
+    ; mov rax, 0          ; read syscall number
+    mov rdx, 24         ; size to read
+    syscall
+
+    ; write output
+    mov rax, 1          ; write syscall
+    mov rdi, 1          ; set fd to stdout
+    mov rdx, 24         ; size to read
+    syscall
+
+    ; exit
+    ; do we really care about a nice exit?
+    ;mov rax, 60
+    ;mov rdi, 0
+    ;syscall
+
